79bed78ff4d4002c2df0648397d77cdf14616554
eliza-juodvalkyte <ejuodvalkyte@gmail.com>
2024-07-13 20:59:11 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const Handlebars = require("handlebars");
+const fs = require("fs");
+
+module.exports = {
+  devServer: {
+    static: "./dist",
+    open: true,
+    port: 3000,
+    watchFiles: ["src/**/*"],
+  },
+  entry: "./main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.hbs$/i,
+        loader: "html-loader",
+        options: {
+          preprocessor: (content, loaderContext) => {
+            let result;
+
+            const componentsDir = path.join(__dirname, "src/components");
+
+            fs.readdirSync(componentsDir).forEach((file) => {
+              const fullPath = path.join(componentsDir, file);
+
+              loaderContext.addDependency(fullPath);
+
+              const componentContent = fs.readFileSync(fullPath, "utf8");
+              const componentName = path.basename(file, path.extname(file));
+
+              Handlebars.registerPartial(componentName, componentContent);
+            });
+            try {
+              result = Handlebars.compile(content)();
+            } catch (error) {
+              loaderContext.emitError(error);
+
+              return content;
+            }
+
+            return result;
+          },
+        },
+      },
+      {
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.hbs",
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
+};
